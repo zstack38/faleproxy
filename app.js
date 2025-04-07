9c49b12d11b36c03ad6b2ebde1add9fc40922836
Zach Stack <zstack@college.harvard.edu>
2025-04-07 15:42:10 -0400

    Test changing app to make yml pick up error and dont deploy

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post('/fetch', async (req, res) => {
     });
     
     // Process title separately
-    const title = $('title').text().replace(/Yale/g, 'Fale').replace(/yale/g, 'fale').replace(/YALE/g, 'FALE');
+    const title = $('title').text()
     $('title').text(title);
     
     return res.json({ 
